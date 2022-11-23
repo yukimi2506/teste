754a0212418c55a4d44eb5eb82ca2755e202c7fc
yukimi2506 <96307440+yukimi2506@users.noreply.github.com>
2022-11-23 15:22:36 -0400

    Add files via upload

---
+++ sender.py
@@ -0,0 +1,11 @@
+import socket
+
+MCAST_GRP = '224.1.1.1'
+MCAST_PORT = 2424
+
+MULTICAST_TTL = 2
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, MULTICAST_TTL)
+
+sock.sendto(b"Hello World", (MCAST_GRP, MCAST_PORT))
